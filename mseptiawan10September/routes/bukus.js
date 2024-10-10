f4490824351475a2ff28ab64ea3b38895a312b85
mseptiawan <mseptiawan017@gmail.com>
2024-10-10 12:59:17 +0700

    september
    terhubung ke mongodb

---
+++ mseptiawan10September/routes/bukus.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+
+
+const Buku = require("../model/buku");
+/* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.send('respond dari BUKU router');
+// });
+
+
+
+
+
+
+//format JSON
+router.post('/', (req, res) => {
+    const buku = new Buku({
+        judul : req.body.judul,
+        penulis : req.body.penulis,
+        genre : req.body.genre
+    });
+
+
+    console.log(buku);
+    buku.save().then((createdBuku)=>{
+        res.status(201).json({
+                message : "Data berhasil disimpan",
+                bookId : createdBuku._id
+        });
+    });
+   
+});
+
+
+
+
+module.exports = router;
+
+
